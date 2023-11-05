a3ca1cacd13148403f99c4217a8664e28bec6dc0
Brandon-Duffy <97912287+Brandon-Duffy@users.noreply.github.com>
2023-11-05 18:37:44 -0500

    Add files via upload
    Homework week 10 and class code as well.

---
+++ Week 10/getservicelogs.ps1
@@ -0,0 +1,86 @@
+﻿function select_log() {
+    
+    cls
+
+    # List all the event logs
+    $theLogs = Get-EventLog -list | select Log
+    $TheLogs | Out-Host
+
+    # Initialize the array to store the logs
+    $arrLog = @()
+
+    foreach ($tempLog in $theLogs) {
+
+        # Add each log to the array
+        # note, these are stored in the array as a hashtable in the format:
+        # @{Log=LOGNAME}
+        $arrLog += $tempLog
+    }
+
+    # Test to be sure our array is being populated
+    write-host $arrLog[0]
+
+    # Prompt the user for the log to view or quit.
+    $readLog = read-host -Prompt "please enter a log from the list above or 'q' to quit the program"
+
+    # check if the user wants to quit.
+    if ($readLog -match "^[qQ]$") {
+
+        # stop executing the program and close the script
+        break
+
+    }
+
+    log_check -logToSearch $readLog
+
+} # ends the select_log()
+
+
+function log_check() {
+
+    # String the user types in within the select_log function
+    Param([string]$logToSearch)
+
+    # format the user input.
+    # example: @{Log=glassbottle}
+    $theLog = "^@{Log=" + $logToSearch + "}$"
+
+    # search the array for the exact hashtable string
+    if ($arrLog -match $theLog){
+
+        write-host -BackgroundColor Green -ForegroundColor white "Please wait, it may take a few moments to retrieve the log entries"
+        sleep 2
+
+        # call the function to view the log. 
+        view_log -logToSearch $logToSearch
+
+    } else {
+        write-host -BackgroundColor red -ForegroundColor white "the log specified doesnt exist"
+        sleep 2
+        select_log
+
+    }
+
+}
+
+
+function view_log() {
+
+    cls
+
+    # Get the logs 
+    Get-EventLog -Log $logToSearch -Newest 10 -after "1/18/2020"
+
+    # pause the screen and wait until the user is ready to proceed.
+    read-host -Prompt "press enter when you are done"
+
+    # go back to select_log
+    select_log
+
+} # ends the view_log()
+
+# run the select_log as the first function
+select_log
+
+
+
---
+++ Week 10/homeworkWeek10.ps1
@@ -0,0 +1,55 @@
+﻿function select_service_status() {
+    do {
+        cls
+        # Prompt the user for the service status to view or quit.
+        $serviceStatus = Read-Host -Prompt "Enter 'all' to view all services, 'stopped' for only stopped services, 'running' for running services, or 'q' to quit the program"
+
+        # Check if the user wants to quit.
+        if ($serviceStatus -eq 'q') {
+            Write-Host "Exiting the program..."
+            return
+        }
+        # DID NOT USE THE TIP
+        # Define valid inputs
+        $validInputs = @('all', 'stopped', 'running')
+
+        # Check that the user specified only 'all', 'stopped', or 'running' as a value
+        if ($serviceStatus -in $validInputs) {
+            # Call the function to display the services based on the user's choice
+            display_services -status $serviceStatus
+        } else {
+            Write-Host -BackgroundColor Red -ForegroundColor White "Invalid input, please enter 'all', 'stopped', or 'running'."
+            Start-Sleep -Seconds 2
+        }
+    } while ($serviceStatus -notin $validInputs)
+}
+
+function display_services() {
+    param([string]$status)
+
+    cls
+
+    switch ($status) {
+        'all' {
+            $services = Get-Service
+            break
+        }
+        'stopped' {
+            $services = Get-Service | Where-Object { $_.Status -eq 'Stopped' }
+            break
+        }
+        'running' {
+            $services = Get-Service | Where-Object { $_.Status -eq 'Running' }
+            break
+        }
+    }
+
+    # display the services
+    $services | Format-Table -Property DisplayName, Status
+
+    # pause the screen and wait until the user is ready to proceed.
+    Read-Host -Prompt "Press Enter when you are done"
+}
+
+# call the function. 
+select_service_status
