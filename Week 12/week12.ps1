fc48540c1b6e7a076d3b4b687e5ec6207a1ba913
Brandon-Duffy <97912287+Brandon-Duffy@users.noreply.github.com>
2023-11-28 20:04:28 -0500

    Add files via upload

---
+++ Week 12/week12.ps1
@@ -0,0 +1,21 @@
+﻿# Login to a remote SSH server
+Import-Module Posh-SSH
+New-SSHSession -ComputerName '192.168.50.24' -Credential (Get-Credential champuser)
+
+<#
+while ($true) {
+
+# Add a prompt to run commands
+
+$the_cmd = read-host -Prompt "please enter a command"
+
+# Run command on remote SSH server
+
+Invoke-SSHCommand -index 0 $the_cmd).Output
+
+
+}
+#>
+
+Set-SCPFile -ComputerName '192.168.50.24' -Credential (Get-credential champuser) `
+-RemotePath '/home/Desktop' -LocalFile 'C:\Users\champuser\Desktop\cat.jpg' -Verbose
